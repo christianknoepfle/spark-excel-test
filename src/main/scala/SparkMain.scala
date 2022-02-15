ca356765aba7ffcb4080bd44eb12f263218517fa
Christian Knoepfle <christian.knoepfle@gmx.de>
2022-02-15 19:20:35 +0100

    use spark excel 0.16.4 added partitioning test case some test refactoring

--- src/main/scala/SparkMain.scala
+++ src/main/scala/SparkMain.scala
@@ -10,63 +10,7 @@ object SparkMain {
         .appName("Simple Application")
         .getOrCreate()
 
-    println("use v1")
-    excelV1(spark)
-    println("use v2")
-    excelV2(spark)
     spark.stop()
   }
 
-  private def excelV1(spark: SparkSession) = {
-    // read the provided csv then write it using crealytics v1 api
-    // then read it back and print content
-    val dfCsv = spark.read
-      .format("simple_csv")
-      .option("delimiter", ";")
-      .option("header", "true")
-      .option("path", "src/main/resources/simple.csv")
-      .load()
-
-    dfCsv.write
-      .format("com.crealytics.spark.excel")
-      .option("path", "tmp/v1/test_simple.xlsx")
-      .option("header", true)
-      .mode(SaveMode.Overwrite)
-      .save()
-
-    val dfExcel = spark.read
-      .format("com.crealytics.spark.excel")
-      //.option("path", "src/main/resources/test_simple.xlsx")
-      .option("path", "tmp/v1/test_simple.xlsx")
-      .option("header", true)
-      .load()
-
-    dfExcel.show()
-  }
-
-  private def excelV2(spark: SparkSession) = {
-    // read the provided csv then write it using crealytics v1 api
-    // then read it back and print content
-    val dfCsv = spark.read
-      .format("simple_csv")
-      .option("delimiter", ";")
-      .option("header", "true")
-      .option("path", "src/main/resources/simple.csv")
-      .load()
-
-    dfCsv.write
-      .format("excel")
-      .option("path", "tmp/v2/")
-      .option("header", true)
-      .mode(SaveMode.Overwrite)
-      .save()
-
-    val dfExcel = spark.read
-      .format("excel")
-      .option("path", "tmp/v2/*.xlsx")
-      .option("header", true)
-      .load()
-
-    dfExcel.show()
-  }
 }
--- src/test/scala/ExcelReadWriteTest.scala
+++ src/test/scala/ExcelReadWriteTest.scala
@@ -6,6 +6,15 @@ import java.io.File
 
 class ExcelReadWriteTest extends AnyWordSpec with SparkTestApp {
 
+  def getListOfFiles(dir: String): List[File] = {
+    val d = new File(dir)
+    if (d.exists && d.isDirectory) {
+      d.listFiles.filter(_.isFile).toList
+    } else {
+      List[File]()
+    }
+  }
+
   def deleteDirectory(directoryToBeDeleted: File): Boolean = {
     val allContents = directoryToBeDeleted.listFiles
     if (allContents != null) for (file <- allContents) {
@@ -32,6 +41,20 @@ class ExcelReadWriteTest extends AnyWordSpec with SparkTestApp {
     fixture
   }
 
+  private def getExcelFileList(targetDir: String): Seq[String] = {
+    val filesInTargetDir = getListOfFiles(targetDir)
+    filesInTargetDir.filter(_.getName.endsWith("xlsx")).map(_.getName)
+  }
+
+  private def readSimpleCsv = {
+    spark.read
+      .format("csv")
+      .option("delimiter", ";")
+      .option("header", "true")
+      .option("path", "src/main/resources/simple_csv/simple.csv")
+      .load()
+  }
+
   "excel v1" can {
     "read a dataframe from xlsx" in {
       val dfExcel = spark.read
@@ -46,15 +69,9 @@ class ExcelReadWriteTest extends AnyWordSpec with SparkTestApp {
 
     "write a dataframe to xlsx" in withExistingCleanTempDir("v1") { targetDir =>
       // create a df from csv then write as xlsx
-      val dfCsv = spark.read
-        .format("csv")
-        .option("delimiter", ";")
-        .option("header", "true")
-        .option("path", "src/main/resources/simple_csv")
-        .load()
+      val dfCsv = readSimpleCsv
 
       val targetFile = s"${targetDir}/test_simple.xlsx"
-      println(s"Trying to write to $targetFile using V1 API")
       dfCsv.write
         .format("com.crealytics.spark.excel")
         .option("path", targetFile)
@@ -80,25 +97,44 @@ class ExcelReadWriteTest extends AnyWordSpec with SparkTestApp {
       assert(dfExcel.count() == 2)
     }
 
-    "write a dataframe to xlsx" in withExistingCleanTempDir("v2") { targetDir =>
-      // create a df from csv then write as xlsx
-      val dfCsv = spark.read
-        .format("csv")
-        .option("delimiter", ";")
-        .option("header", "true")
-        .option("path", "src/main/resources/simple_csv")
-        .load()
+    val writeModes = Seq(SaveMode.Overwrite, SaveMode.Append)
 
-      dfCsv.write
-        .format("excel")
-        .option("path", targetDir)
-        .option("header", true)
-        .mode(SaveMode.Overwrite)
-        .save()
+    for (writeMode <- writeModes) {
+      s"write a dataframe to xlsx with ${writeMode.toString}" in withExistingCleanTempDir(
+        "v2"
+      ) { targetDir =>
+        // create a df from csv then write as xlsx
+        val dfCsv = readSimpleCsv
 
-      // simple check that we wrote a file
-      val filesInTargetDir = Files.list(Paths.get(targetDir))
-      assert(filesInTargetDir.count() == 1)
+        dfCsv.write
+          .format("excel")
+          .option("path", targetDir)
+          .option("header", true)
+          .mode(writeMode)
+          .save()
+
+        val listOfFiles = getExcelFileList(targetDir)
+        assert(listOfFiles.length == 1)
+      }
+
+      s"write a dataframe to xlsx with ${writeMode.toString} (partitioned)" in withExistingCleanTempDir(
+        "v2"
+      ) { targetDir =>
+        // create a df from csv then write as xlsx
+        val dfCsv = readSimpleCsv
+
+        dfCsv.write
+          .partitionBy("col1")
+          .format("excel")
+          .option("path", targetDir)
+          .option("header", true)
+          .mode(writeMode)
+          .save()
+
+        val listOfFiles = getExcelFileList(targetDir)
+        println(listOfFiles)
+        assert(listOfFiles.length == 3)
+      }
     }
   }
 }
